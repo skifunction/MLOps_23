4f59d6eaf585c23d9a3b3cdd94106bc79cc64f23
Sujay Kumar Ingle <skifunction@skifunction.localdomain>
2023-10-21 13:33:05 +0530

    Quiz-2

--- digit.py
+++ digit.py
@@ -8,13 +8,20 @@ data = digits.images.reshape((n_samples, -1))
 gamma = [1, 0.1, 0.01, 0.001, 0.0001, 0.00001]
 c = [1, 1.02, 1.04, 1.07, 1.081]
 
-possible_combination = []
+max_depth = [5, 10, 15]
+min_samples_split = [2, 3, 4, 5]
 
-possible_combination.append(gamma)
-possible_combination.append(c)
+possible_combination_SVM = []
+possible_combination_DT = []
 
-test_size = [0.1, 0.2, 0.3]
-dev_size = [0.1, 0.2, 0.3]
+possible_combination_SVM.append(gamma)
+possible_combination_SVM.append(c)
+
+possible_combination_DT.append(max_depth)
+possible_combination_DT.append(min_samples_split)
+
+test_size = [0.2]
+dev_size = [0.2]
 
 for test_s in test_size:
     for dev_s in dev_size:
@@ -22,7 +29,11 @@ for test_s in test_size:
 
         X_train, X_test, y_train, y_test, dev_x, dev_y = split_train_dev_test(data, digits.target, test_s, dev_s)
         
-        best_hparams, best_model, best_accuracy = tune_hparams(X_train, y_train, dev_x, dev_y, possible_combination)
+        best_hparams, best_model, best_accuracy = tune_hparams_SVM(X_train, y_train, dev_x, dev_y, possible_combination_SVM)
+        best_hparams_DT, best_model_DT, best_accuracy_DT = tune_hparams_DT(X_train, y_train, dev_x, dev_y, possible_combination_DT)
         
-        print(f"test_size={test_s} dev_size={dev_s} train_size={train_s:.0f} train_acc={best_accuracy:.2f} dev_acc={best_accuracy:.2f} test_acc={best_accuracy:.2f}")
-        print(f"Best Hyperparameters for this run: ( gamma : {best_hparams[0]} , C : {best_hparams[1]} )")
+        print(f"For SVM test_size={test_s} dev_size={dev_s} train_size={train_s:.0f} train_acc={best_accuracy:.2f} dev_acc={best_accuracy:.2f} test_acc={best_accuracy:.2f}")
+        print(f"Best Hyperparameters for SVM this run: ( gamma : {best_hparams[0]} , C : {best_hparams[1]} )")
+        
+        print(f"For decision tree test_size={test_s} dev_size={dev_s} train_size={train_s:.0f} train_acc={best_accuracy_DT:.2f} dev_acc={best_accuracy_DT:.2f} test_acc={best_accuracy:.2f}")
+        print(f"Best Hyperparameters for Decision tree this run: ( criterion : {best_hparams_DT[0]} , Splitter : {best_hparams_DT[1]} )")
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from sklearn import datasets, metrics, svm
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 
 def data():
@@ -25,12 +26,17 @@ def training_data(x, y, parameters):
     clf.fit(x, y)
     return clf
 
+def Decision_tree(x, y, parameters):
+    clf = DecisionTreeClassifier(max_depth=parameters['max_depth'], min_samples_split=parameters['min_samples_split'])
+    clf.fit(x, y)
+    return clf
+
 def predict_and_eval(model, X_test, y_test):
     predicted = model.predict(X_test)
     evaluation = metrics.accuracy_score(y_pred = predicted, y_true=y_test)
     return evaluation
 
-def tune_hparams(X_train, y_train, dev_x, dev_y, list_of_all_param_combination):
+def tune_hparams_SVM(X_train, y_train, dev_x, dev_y, list_of_all_param_combination):
     best_accuracy = -1
     best_model=None
     best_hparams = None
@@ -48,3 +54,20 @@ def tune_hparams(X_train, y_train, dev_x, dev_y, list_of_all_param_combination):
     
     return best_hparams, best_model, best_accuracy  
     
+def tune_hparams_DT(X_train, y_train, dev_x, dev_y, list_of_all_param_combination):
+    best_accuracy = -1
+    best_model=None
+    best_hparams = None
+
+    for i in list_of_all_param_combination[0]:
+        for j in list_of_all_param_combination[1]:
+            model = Decision_tree(X_train, y_train, {'max_depth': i,'min_samples_split': j})
+
+            val_accuracy = predict_and_eval(model, dev_x, dev_y)
+            
+            if val_accuracy > best_accuracy:
+                best_hparams = [i, j]
+                best_accuracy = val_accuracy
+                best_model = model
+    
+    return best_hparams, best_model, best_accuracy 
